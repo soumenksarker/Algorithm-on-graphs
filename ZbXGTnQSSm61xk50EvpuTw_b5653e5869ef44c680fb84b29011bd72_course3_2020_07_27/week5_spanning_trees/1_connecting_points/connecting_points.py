910c58a28490be3449db40e57aa7c1b277d8b2b0
Soumen Sarker <87926557+soumenksarker@users.noreply.github.com>
2022-11-30 01:45:50 +0600

    Update connecting_points.py

--- ZbXGTnQSSm61xk50EvpuTw_b5653e5869ef44c680fb84b29011bd72_course3_2020_07_27/week5_spanning_trees/1_connecting_points/connecting_points.py
+++ ZbXGTnQSSm61xk50EvpuTw_b5653e5869ef44c680fb84b29011bd72_course3_2020_07_27/week5_spanning_trees/1_connecting_points/connecting_points.py
@@ -1,16 +1,48 @@
-#Uses python3
-import sys
+# Uses python3
+#import sys
 import math
-
+def distance(v1, v2, x, y):
+    return math.sqrt((x[v1] - x[v2]) ** 2 + (y[v1] - y[v2]) ** 2)
 def minimum_distance(x, y):
-    result = 0.
-    #write your code here
-    return result
+    # create edge list with possible distance in pairs of nodes
+    edges = []
+    for i in range(n):
+        for j in range(i, n):
+            if i != j:
+                edges.append([i, j, distance(i, j, x, y)])
+    #print(edges)
+    # sort edges by distances
+    sorted_Edges = sorted(edges, key=lambda tup: tup[2])
+    # for ind,i in enumerate(sorted_Edges):
+    #    print(i)
+    # print()
 
-
+    # initialize disjoint data structure, we'll do union if there's edge between two nodes before
+    membership = range(n)
+    # for i in membership:
+    #    print(i)
+    # print()
+    # run Kruskal algorithm
+    MST = []  # initialize minimum spanning tree
+    minDist = 0
+    for i in sorted_Edges:
+        # make sure vertices are not already joined, or in same masks
+        if membership[i[0]] != membership[i[1]]:
+            # add edge
+            MST.append(i)
+            minDist += i[2]
+            # join/union groups
+            membership = list(map(lambda x: membership[i[0]] if x == membership[i[1]] else x, membership))
+            #print(membership)
+    # print()
+    # for i in MST:
+    #    print(i)
+    # print()
+    # write your code here
+    return minDist
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    #input = sys.stdin.read()
+    data = list(map(int, input().split()))
     n = data[0]
     x = data[1::2]
     y = data[2::2]
